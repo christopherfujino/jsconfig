e70b110cdd02c3fa0322f180f45a2b1349e454b3
Christopher Fujino <christopherfujino@gmail.com>
2023-12-24 02:39:31 -0800

    clean up

--- bin/main.ml
+++ bin/main.ml
@@ -7,8 +7,9 @@ let program = "{\
 
 let rec print_expr e = match e with
   | Object (k, v) -> "{\"" ^ k ^ "\": " ^ print_expr v ^ "}"
-  | Array [] -> "[]"
-  | Array (h :: t) -> "[" ^ print_expr h ^ ", " ^ print_expr (Array t) ^ "]"
+  | Array a -> (match a with
+    | [] -> "[]"
+    | h :: t -> "[" ^ print_expr h ^ ", " ^ print_expr (Array t) ^ "]")
   | String s -> "\"" ^ s ^ "\""
   | Number n -> string_of_float n
 
